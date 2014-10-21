8e8f05ae7187ab45c19b57275ec3f0a268f01406
Peihong Guo <peihongguo@gmail.com>
2014-10-21 13:38:48 -0500

    format

--- guo_hw3/path.pl
+++ guo_hw3/path.pl
@@ -5,29 +5,29 @@ arc(k,q,3).
 
 % degenerate case
 route(A,B,Visited,Path,L) :-
-       arc(A,B,L),
-	   append(Visited, [A, B], Path).
+  arc(A,B,L),
+  append(Visited, [A, B], Path).
 
 % general case
 route(A,B,Visited,Path,L) :-
-       arc(A,C,Dist),
-       C \== B,
-       \+member(C,Visited),
-	   append(Visited, [A], NewVisited),
-       route(C,B,NewVisited,Path,Lp),
-       L is Dist+Lp.
+  arc(A,C,Dist),
+  C \== B,
+  \+member(C,Visited),
+  append(Visited, [A], NewVisited),
+  route(C,B,NewVisited,Path,Lp),
+  L is Dist+Lp.
 
 % from a set of paths, find the shortest one
 shortest([Head|Tail],M) :- shortestRoute(Tail,Head,M).
 shortestRoute([],M,M).
 shortestRoute([Head|Tail], CurMin, Min) :- 
-		Head = [_|L], 
-		CurMin = [_|Lmin], 
-		L < Lmin, !, shortestRoute(Tail, Head, Min).
+  Head = [_|L], 
+  CurMin = [_|Lmin], 
+  L < Lmin, !, shortestRoute(Tail, Head, Min).
 shortestRoute([_|Tail],CurMin,Min) :- shortestRoute(Tail,CurMin,Min).
 
 % driver rule
 path(A,B,Path) :-
-   setof([Path, Len],route(A,B,[],Path,Len), PathSet),	% the set of all paths
-   PathSet \== [], % the set of path must not be empty
-   shortest(PathSet,[Path, Len]).
+  setof([Path, Len],route(A,B,[],Path,Len), PathSet),	% the set of all paths
+  PathSet \== [], % the set of path must not be empty
+  shortest(PathSet,[Path, Len]).
